05a18ee2f6455b623fed52d37ab4156303435f6d
tatsukawa0353 <s.tatsukawa@biomech.me.es.osaka-u.ac.jp>
2025-04-11 14:43:56 +0900

    お勉強

--- kadai3_tatsu.cpp
+++ kadai3_tatsu.cpp
@@ -1,4 +1,4 @@
-#include <iostream> //c++において画面表示の入出力を扱うライブラリ(i:input,o:output
+#include <iostream> //c++において画面表示の入出力を扱うライブラリ(i:input,o:output）
 #include <fstream> //ファイル操作用(今回の場合CSVの読み込み）
 #include <Eigen/Dense> //Eigen使用時に書く，数列・行列計算ライブラリ
 #include <string> //文字列の道具,std::stringを使用するために
@@ -8,19 +8,20 @@ using namespace Eigen; //いちいちstd::とか書かんでええおまじな�
 
 // CSVから行列を読み込む
 MatrixXd readMatrixCSV(const string& filename, int rows, int cols) {  //CSVから行列を読み込む関数，MatrixXdはEigenの行列データ型，filenameはファイル名取得？，rows,colsは行列大きさの取得（今回は100*100）
+   //int:整数型のデータ，const:これは変更しないの意,string&:文字列を「参照」で渡す．→const string&:文字列を参照で渡すけど、関数の中では変更できない．速くて安全
     MatrixXd mat(rows, cols); //空行列をまず作り，後で中身をcsvから詰める
     ifstream file(filename); //ファイルを開く,読み取り専用，ifstreamはファイル内読み込みの際に
     string line;
     int row = 0; //1行ずつ読んでいくための変数用意
 
     while (getline(file, line) && row < rows) { //上限のrows行までcsvファイルを1行ずつ読み込むループ
-        stringstream ss(line);
-        string cell;
-        int col = 0; //読み込んだ1行をカンマで分割する準備
+        stringstream ss(line); //stringstream:文字列から数値を抜き取る・区切って取り出す
+        string cell; //1つずつ取り出した「カンマの間の値（文字列）」を入れる箱(cell:csvの1マス）
+        int col = 0; //読み込んだ1行をカンマで分割する準備,ここは今何列目にいるのかカウントする変数
 
-        while (getline(ss, cell, ',') && col < cols) {
-            mat(row, col) = stod(cell);
-            col++;
+        while (getline(ss, cell, ',') && col < cols) {  //ss からカンマ , で区切って、1個ずつ cell に取り出す.＆＆以降は「列数より多く読まない」ようにしてる
+            mat(row, col) = stod(cell);  //Eigenの行列の「row 行 col 列」に値を入れる.stod(cell)は文字列をdouble型に変換
+            col++;  //次列へ
         } //カンマで区切られた数字を1つずつ読み込んで行列に詰めていく
         row++; //次の行へ
     }
